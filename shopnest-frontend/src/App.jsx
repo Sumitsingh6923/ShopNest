cfa2bc5105c8de40df85c36e39beef4f83e3b3b9
Sumit Singh <singh.sumit.9623@gmail.com>
2026-05-26 20:34:16 +0530

    commit initial file

--- shopnest-frontend/src/App.jsx
+++ shopnest-frontend/src/App.jsx
@@ -8,11 +8,11 @@ import {
 import { useSelector } from "react-redux";
 
 import Home from "./pages/Home/Home";
-import Register from "./pages/login/Register";
-import Login from "./pages/login/Login";
+import Register from "./pages/Login/Register";
+import Login from "./pages/Login/Login";
 import Cart from "./pages/Cart/Cart";
-import UserOrder from "./pages/order/UserOrder";
-import Checkout from "./pages/checkout/CheckOut";
+import UserOrder from "./pages/Order/UserOrder";
+import Checkout from "./pages/Checkout/CheckOut";
 
 import AdminDashboard from "./admin/pages/AdminDashBoard";
 import EditProduct from "./admin/pages/EditProduct";
